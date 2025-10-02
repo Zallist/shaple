a9d7a6e0a6bff527baaf7bc38ebe0bc6fed8a49b
Zallist <Zallist@gmail.com>
2025-10-02 17:49:12 +0100

    Update Shaple.tsx

--- src/Shaple.tsx
+++ src/Shaple.tsx
@@ -384,7 +384,7 @@ export default function App() {
                           case 'absent':
                             return <span>⬛</span>;
                           case 'invalid_reused':
-                            return <span>⬛</span>;
+                            return <span>🟧</span>;
                         }
                       });
 
