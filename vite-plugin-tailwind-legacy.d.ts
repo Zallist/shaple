2f31765787a04f20330e4d4c721da309b5fc7c30
Dan Whittaker <zallist@gmail.com>
2025-10-03 12:32:28 +0100

    Remove all unnecessary overrides for legacy mode

--- vite-plugin-tailwind-legacy.d.ts
+++ vite-plugin-tailwind-legacy.d.ts
@@ -1,15 +1,13 @@
-declare module 'vite-plugin-tailwind-legacy' {
-    import { Plugin } from 'vite';
-    
-    interface TailwindLegacyOptions {
-      tailwindConfig?: string;
-      inputCSS?: string,
-      assetsDir?: string;
-      publicPath?: string;
-      buildDir?: string;
-      injectInHTML?: boolean;
-    }
-  
-    const TailwindLegacyPlugin: (options?: TailwindLegacyOptions) => Plugin;
-    export default TailwindLegacyPlugin;
-  }
+import { Plugin } from 'vite';
+
+interface TailwindLegacyOptions {
+  tailwindConfig?: string;
+  inputCSS?: string;
+  assetsDir?: string;
+  publicPath?: string;
+  buildDir?: string;
+  injectInHTML?: boolean;
+}
+
+declare const TailwindLegacyPlugin: (options?: TailwindLegacyOptions) => Plugin;
+export default TailwindLegacyPlugin;
